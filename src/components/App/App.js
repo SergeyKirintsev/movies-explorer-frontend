dcf0168939f00fcf1c91133bd0e554e35b306400
SergeyKirintsev <kserg80@gmail.com>
2021-09-26 12:27:49 +0300

    Создать компонент Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from '../Header/Header';
 import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer/>
     </>
   );
 }
