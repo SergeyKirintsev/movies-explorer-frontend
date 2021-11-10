8423ef1fa59bc8ea834e52d3b87e31871bfcf6f2
SergeyKirintsev <kserg80@gmail.com>
2021-11-10 23:52:05 +0300

    Создать Context

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Route, Switch} from "react-router-dom";
 import './App.css';
 import Main from "../Main/Main";
@@ -7,38 +7,49 @@ import SavedMovies from "../SavedMovies/SavedMovies";
 import Profile from "../Profile/Profile";
 import Register from "../Register/Register";
 import Login from "../Login/Login";
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
 function App() {
   const menuState = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    setCurrentUser({
+      name: "Sergey",
+      email: "kserg80@gmail.com"
+    })
+  }, [])
 
   return (
-    <>
-      <Switch>
-        <Route path='/' exact>
-          <Main/>
-        </Route>
+    <CurrentUserContext.Provider value={currentUser}>
+      <>
+        <Switch>
+          <Route path='/' exact>
+            <Main/>
+          </Route>
 
-        <Route path='/movies'>
-          <Movies menuState={menuState}/>
-        </Route>
+          <Route path='/movies'>
+            <Movies menuState={menuState}/>
+          </Route>
 
-        <Route path='/saved-movies'>
-          <SavedMovies menuState={menuState}/>
-        </Route>
+          <Route path='/saved-movies'>
+            <SavedMovies menuState={menuState}/>
+          </Route>
 
-        <Route path='/profile'>
-          <Profile menuState={menuState} />
-        </Route>
+          <Route path='/profile'>
+            <Profile menuState={menuState} />
+          </Route>
 
-        <Route path='/sign-up'>
-          <Register />
-        </Route>
+          <Route path='/sign-up'>
+            <Register />
+          </Route>
 
-        <Route path="/sign-in">
-          <Login />
-        </Route>
-      </Switch>
-    </>
+          <Route path="/sign-in">
+            <Login />
+          </Route>
+        </Switch>
+      </>
+    </CurrentUserContext.Provider>
   );
 }
 
